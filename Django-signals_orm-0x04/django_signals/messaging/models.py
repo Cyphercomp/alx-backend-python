5bb892f4dd4ffe2be45f08e7211e448c1269b104
Cyphercomp <ndicunguyecedric@gmail.com>
2025-06-29 11:34:56 -0700

    added related name

--- Django-signals_orm-0x04/django_signals/messaging/models.py
+++ Django-signals_orm-0x04/django_signals/messaging/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE , related_name='sender')
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE , related_name='receipient')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE , related_name='sent_message')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE , related_name='recereceived_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
